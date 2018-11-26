3c214f69c6be2b1d4814c79f68b0035e50777800
Nehil Spirit <nehil.spirit0307@gmail.com>
2018-11-26 13:31:41 +0200

    Добавлены метки и модальные окна обьявлений

--- js/map.js
+++ js/map.js
@@ -34,7 +34,6 @@ var offerTitle = [
 
 var checkinCheckout = ["12:00", "13:00", "14:00"];
 
-       // убрала класс панель не показывается((
 
 //элемент, в который  будем вставлять похожиe метки.
 var similarListElement = document.querySelector(".map__pins");
@@ -53,61 +52,76 @@ var similarCardTamplate = document
 
 /* элемент перед которым нужно вставить карточки */
 var before = document.querySelector(".map__filters-container");
+
 /* создает пустой контейнер для шаблонов */
 var fragment = document.createDocumentFragment();
 
 /*убирает класс */
 var userMap = document.querySelector(".map.map--faded");
- userMap.classList.remove(".map--faded");
- //var test=userMap.classList.contains(".map--faded"); // не работает хотя тест дал false
-
+ userMap.classList.remove("map--faded");
 
 // Вычисляет случайное чисто в диапазоне между min - max
 function getRandomNum(min, max) {
   return Math.round(Math.random() * (max - min) + min); // ну тут все ясно и так)
 }
 
-var getRandom = function(arr) {
+// @oldfox переименуйте в getRandomElement или что-то похожее
+var getRandomElement = function(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 };
 
-
+// @oldfox перемешивание Фишер-Йетс
+var shuffle = function(array) {
+  for (var i = array.length - 1; i > 0; i--) {
+    var randomNumber = getRandomNum(0, i);
+    var getRandomElement = array[randomNumber];
+    array[randomNumber] = array[i];
+    array[i] = getRandomElement;
+  }
+  return array;
+};
 
 // создаем рандомное объявление
-var createInfo = function(num) {
-    // можно создавать объект одновременно с присвоением его свойствам значений
-    var result = {};
-    result = {
+var createInfo = function(num) {    
+  
+    var location = {
+      x: getRandomNum(0, similarListCardElement.offsetWidth),
+      y: getRandomNum(130, 630)
+    };
+
+    return  {
         author: {
-      avatar: "img/avsatars/user0" + (num + 1) + ".png"
+      avatar: "img/avatars/user0" + (num + 1) + ".png"
       },
       location: {
-        x: getRandomNum(640, 750),
-        y: getRandomNum(640, 750)
+        x: location.x,
+        y: location.y
       },
       offer: {
         title: offerTitle[num],
         price: getRandomNum(1000, 1000000),
-        address: location.x + ', ' + location.y,  // эта строка не работает
-        kind: getRandom(offerKind),
+        address: location.x + ', ' + location.y,
+        kind: getRandomElement(offerKind),
         rooms: getRandomNum(1, 5),
         guests: getRandomNum(1, 10),
-        checkin: getRandom(checkinCheckout),
-        checkout: getRandom(checkinCheckout),
-        features:  offerFeatures,
+        checkin: getRandomElement(checkinCheckout),
+        checkout: getRandomElement(checkinCheckout),
+        // массив строк случайной длины из ниже предложенных
+        features: offerFeatures.slice(0, getRandomNum(1, offerFeatures.length)),
         description: '',
-        photos: offerPhotos
+        // массив из строк ... расположенных в произвольном порядке
+        photos: shuffle(offerPhotos)
            }
-     }
-     return result
+     };
 };
+
 // наполнение массива
 var listPins = [];//
 var createInfoArray = function(num){
   for(var i=0; i<num; i++){
    listPins.push(createInfo(i));
   }
-  return listPins;
+  // return listPins;
 };
 
 //наполняем массив
@@ -116,27 +130,24 @@ createInfoArray(ARR_NUM);
 
 
 /*  функция создает шаблон метки и заполняет данными из массива ads */
-
 var createPin = function(item) {
 
   var pinElement = similarPinTemplate.cloneNode(true);
-  //pinElement.querySelector('.map__pin').style.left = item.location.x + 'px';// тут тоже не работает из за строки 91
-  //pinElement.querySelector('.map__pin').style.top = item.location.y + 'px';
+  pinElement.style.left = item.location.x - PIN_WIDTH / 2 + "px";
+  pinElement.style.top = item.location.y - PIN_HEIGHT + "px";
   pinElement.querySelector('img').src = item.author.avatar;
   pinElement.querySelector('img').alt = item.offer.title;
 
   return pinElement;
 };
-
-
 //Отрисoвывает сгенерированные DOM-элементы (метки) в блок .map__pins.
-var appendPin = function(item) {
-  for (var i = 0; i < item.length; i++){
-    fragment.appendChild(createPin(item[i]));
-  }
+var appendPin = function(item){
+  item.forEach(function (elem) {
+   fragment.appendChild(createPin(elem));
+  });
   similarListElement.appendChild(fragment);
-};
-
+  };
+ 
 appendPin(listPins);
 
 /* проверяет значения для вставки в ".popup__type"*/
@@ -158,36 +169,43 @@ var addType = function(obj){
 
 };
 
-/*  функция создает шаблон карточки  и заполняет данными из массива */
-// опять функция длинной в метр, пока не приходит в голову как оптимизтровать
-// очень хотелось, но было страшно сломать
-var createCard = function(array) {
+/*  создает шаблон карточки  и заполняет данными из массива */
+var createCard = function(item) {
+  
   var cardElement = similarCardTamplate.cloneNode(true);
-
-  cardElement.querySelector(".popup__title").textContent = array.offer.title;
+console.log(item);
+  cardElement.querySelector(".popup__title").textContent = item.offer.title;
   cardElement.querySelector(".popup__text--address").textContent =
-  array.offer.address;
+  item.offer.address;
   cardElement.querySelector(".popup__text--price").textContent =
-  array.offer.price + "$//ночь";
-  cardElement.querySelector(".popup__type").textContent = addType(array.offer.kind);
+  item.offer.price + "$//ночь";
+  cardElement.querySelector(".popup__type").textContent = addType(item.offer.kind);
   cardElement.querySelector(".popup__text--capacity").textContent =
-  array.offer.rooms + " комнаты для " + array.offer.guests + " гостей";
+  item.offer.rooms + " комнаты для " + item.offer.guests + " гостей";
   cardElement.querySelector(".popup__text--time").textContent =
-  "Заезд после " + array.offer.checkin + " , выезд до " + array.offer.checkout + ".";
-  cardElement.querySelector(".popup__feature--wifi").textContent = array.offer.features[0];
-  cardElement.querySelector(".popup__feature--dishwasher").textContent = array.offer.features[1];
-  cardElement.querySelector(".popup__feature--parking").textContent = array.offer.features[2];
-  cardElement.querySelector(".popup__feature--washer").textContent = array.offer.features[3];
-  cardElement.querySelector(".popup__feature--elevator").textContent = array.offer.features[4];
-  cardElement.querySelector(".popup__feature--conditioner").textContent = array.offer.features[5];
-  cardElement.querySelector(".popup__description").textContent = array.offer.description;
-  cardElement.querySelector(".popup__photo").src = array.offer.photos[0];
+  "Заезд после " + item.offer.checkin + " , выезд до " + item.offer.checkout + ".";
+  // @oldfox удаляем блок 
+  cardElement.querySelector(".popup__features").innerHTML = "";
+  // @oldfox генерируем и добавляем элементы
+  item.offer.features.forEach(function (element) {
+  // клонируем из template 
+  var blockFeatures = similarCardTamplate.querySelector('.popup__feature--' + element).cloneNode(true);
+  // добавляем в нашу карточку
+    cardElement.querySelector(".popup__features").appendChild(blockFeatures);
+  });
+  
+  cardElement.querySelector(".popup__description").textContent = item.offer.description;
 
-
-  // нужно создать еще два изображения и потожить в них в качестве src  photos[1] и photos[2] как?
-
-// по заданию нужно добавить эту строчку, но если добавляю аватар вообще не отображается . Почему?
-  //cardElement.querySelector(".popup__avatar").src = array.author.avatar;
+  // добавляем по анологии 
+  cardElement.querySelector(".popup__photos").innerHTML = "";
+  // генерируем и добавляем элементы
+  item.offer.photos.forEach(function (photoUrl) {
+  // клонируем из template 
+  var photoTemp = similarCardTamplate.querySelector('.popup__photo').cloneNode(true);
+  photoTemp.src = photoUrl;
+  // добавляем в нашу карточку
+  cardElement.querySelector(".popup__photos").appendChild(photoTemp);
+  });
 
   return cardElement;
 };
@@ -200,13 +218,3 @@ var appendCard = function(item) {
 };
 
 appendCard(listPins[0]);
-
-
-
-
-
-
-
-
-
-
